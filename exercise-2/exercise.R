9cb62706dc4c9f8cba7777dcd92e25e275694e27
Michael Bantle <bantlm@uw.edu>
2018-04-13 14:40:43 -0700

    ch10 participation

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -8,26 +8,29 @@
 library(fueleconomy)
 
 # Install and load the "dplyr" library
-
-
+install.packages("dplyr")
+library("dplyr")
+View(vehicles)
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- select(vehicles, make)
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+number_cars <- distinct(vehicles, make)
+number_cars
+nrow(distinct(vehicles, make))
 
 # Filter the data set for vehicles manufactured in 1997
-
+cars_1997 <- filter(vehicles, year==1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
-
+cars_1997 <- arrange(cars_1997, hwy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
-
+cars_1997 <- mutate(cars_1997, average = (hwy + cty) / 2)
+ 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
